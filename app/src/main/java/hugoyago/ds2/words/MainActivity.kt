758960f18f0115fca84a074765cd4571b59ea43a
Hugofriedrich369 <hungos0223@gmail.com>
2023-05-24 22:20:39 -0300

    [Words] Projeto Finalizado

---
+++ app/src/main/java/hugoyago/ds2/words/MainActivity.kt
@@ -0,0 +1,20 @@
+package hugoyago.ds2.words
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import hugoyago.ds2.words.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        recyclerView = binding.recyclerView
+// Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
+    }
+}
